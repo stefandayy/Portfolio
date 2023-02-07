91896463b937de7e1a9e3a3b42603be5a5fffdbe
= <sted96@gmail.com>
2023-02-07 15:17:24 +0100

    Update ProjectsComponent.jsx

--- PortofolioWeb/my-app/src/components/ProjectsComponent.jsx
+++ PortofolioWeb/my-app/src/components/ProjectsComponent.jsx
@@ -7,8 +7,8 @@ import {
   SiHtml5,
   SiJavascript,
   SiReact,
+  SiRedux,
 } from "react-icons/si";
-
 
 export default function ProjectsComponent() {
   return (
@@ -56,14 +56,14 @@ export default function ProjectsComponent() {
         </Col>
 
         <Col>
-          <Card className="px-2" >
+          <Card className="px-2">
             {/* <Card.Img variant="top" src={wApp} alt="wApp" style={{width:'100p',height:'180',margin:"10px"}} /> */}
             <Card.Body>
               <Card.Title>Weather App</Card.Title>
               <Card.Text>
                 <p>
-                  This is a responsive Weather app made in React.js and Boostrap and i choose the API
-                  from Open Weather.
+                  This is a responsive Weather app made in React.js and Boostrap
+                  and i choose the API from Open Weather.
                 </p>
                 <Badge bg="secondary">
                   React.js <SiReact />
@@ -97,19 +97,41 @@ export default function ProjectsComponent() {
         <Col>
           <Card className="px-2">
             {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
-            <Card.Body >
-              <Card.Title>Card title</Card.Title>
+            <Card.Body>
+              <Card.Title>To Do List</Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                <p>
+                  This a responsive To Do List built in React.js with Redux
+                  Toolkit and Boostrap.
+                </p>
+                <Badge bg="secondary">
+                  React.js <SiReact />
+                </Badge>{" "}
+                <Badge bg="secondary" className="me-1">
+                  Redux <SiRedux />
+                </Badge>
+                <Badge bg="secondary">
+                  Bootstrap <SiBootstrap />
+                </Badge>
               </Card.Text>
-              <Button className="me-3 mb-2" variant="info">
-                Demo
-              </Button>
-              <Button className="mb-2" variant="info">
-                Repository
-              </Button>
+              <a
+                href="https://to-do-list-react-redux-stefandayy.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button className="me-3 mb-2" variant="info">
+                  Demo
+                </Button>
+              </a>
+              <a
+                href="https://github.com/stefandayy/ToDoList-react-redux"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button className="mb-2" variant="info">
+                  Repository
+                </Button>
+              </a>
             </Card.Body>
           </Card>
         </Col>
